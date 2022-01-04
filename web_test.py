fe658a3a6709d64cc6abb95ab2fc62536d5810ff
gaolin1 <jackma84@gmail.com>
2022-01-03 22:50:00 -0500

    modified for real output

--- web_test.py
+++ web_test.py
@@ -14,20 +14,20 @@ def main():
     df = import_df()
     #df = pd.DataFrame(px.data.gapminder())
     try:
-        df = df.set_index("Antibiotic")
+        df = df.set_index("Order Name")
         all_drugs = df.index.unique().tolist()
         container = st.container()
         all = st.checkbox("Select all medications")
         if all:
-            drugs = container.multiselect("Choose antibiotic", options=all_drugs, default=all_drugs, key="4")
+            drugs = container.multiselect("Choose medication", options=all_drugs, default=all_drugs, key="4")
             if not drugs:
-                container.error("Please select at least one antibiotic or use select all")
+                container.error("Please select at least one medication or use select all")
         else:
-            drugs = container.multiselect("Choose antibiotic", options=all_drugs, key="1")
+            drugs = container.multiselect("Choose medication", options=all_drugs, key="1")
             if not drugs:
-                container.error("{Please select at least one antibiotic")
+                container.error("{Please select at least one medication")
         departments_select = get_departments(df, drugs, "Department")
-        locations_select = get_departments(df, drugs, "Location")
+        locations_select = get_departments(df, drugs, "Patient Facility")
         container = st.container()
         all = st.checkbox("Select all departments")
         if all:
@@ -35,20 +35,20 @@ def main():
         else:
             departments = container.multiselect("Choose department", options=departments_select, key="5")
         container = st.container()
-        all = st.checkbox("Select all locations")
+        all = st.checkbox("Select all facilities")
         if all:
-            locations = container.multiselect("Choose location", options=locations_select, default=locations_select, key="3")
+            locations = container.multiselect("Choose facilities", options=locations_select, default=locations_select, key="3")
         else:
-            locations = container.multiselect("Choose location", options=locations_select, key="6")
+            locations = container.multiselect("Choose facilities", options=locations_select, key="6")
         drugs_and_department = merge_list(drugs, departments)
         drugs_and_locations = merge_list(drugs, locations)
         all_merged = merge_list_3(drugs, departments, locations)
         df_filtered = pivot_table(df, drugs, departments, drugs_and_department,
-            locations, drugs_and_locations, all_merged, "Antibiotic", "Department", "Location")
-        st.write("Usage per department/location", df_filtered)
-        total_chart = make_chart(df,all_merged, "Antibiotic",drugs)
+            locations, drugs_and_locations, all_merged, "Order Name", "Department", "Patient Facility")
+        st.write("Usage per department/facility", df_filtered)
+        total_chart = make_chart(df,all_merged, "Order Name",drugs)
         department_chart = make_chart(df, drugs_and_department, "Department",drugs)
-        location_chart = make_chart(df, drugs_and_locations, "Location", drugs)
+        location_chart = make_chart(df, drugs_and_locations, "Patient Facility", drugs)
         st.altair_chart(total_chart, use_container_width=True)
         st.altair_chart(department_chart, use_container_width=True)
         st.altair_chart(location_chart, use_container_width=True)
@@ -59,6 +59,7 @@ def import_df():
     df = st.file_uploader("Upload Epic export", type=["xlsx"])
     try:
         df = pd.read_excel(df)
+        df["Order"] = 1
         return df
     except ValueError:
         pass
@@ -84,48 +85,48 @@ def pivot_table(df, var1, var2, list2, var3, list3, list, col1, col2, col3):
     if var2 != []:
         if var2 and var3 != []:
             plot_df = df[df[[col1, col2, col3]].apply(tuple,1).isin(list)]
-            data = pd.pivot_table(plot_df, values="Amount", index=[col1, col2, col3],
-            columns=["Date"]
+            data = pd.pivot_table(plot_df, values="Order ID", index=[col1, col2, col3],
+            columns=["Start Date"], aggfunc=len, fill_value=0
         )
         else:
             plot_df = df[df[[col1, col2]].apply(tuple,1).isin(list2)]
-            data = pd.pivot_table(plot_df, values="Amount", index=[col1, col2],
-            columns=["Date"]
+            data = pd.pivot_table(plot_df, values="Order ID", index=[col1, col2],
+            columns=["Start Date"], aggfunc=len, fill_value=0
         )
     elif var3 != []:
         plot_df = df[df[[col1, col3]].apply(tuple,1).isin(list3)]
-        data = pd.pivot_table(plot_df, values="Amount", index=[col1, col3],
-            columns=["Date"]
+        data = pd.pivot_table(plot_df, values="Order ID", index=[col1, col3],
+            columns=["Start Date"], aggfunc=len, fill_value=0
         )
     else:
         plot_df = df[df[col1].isin(var1)]
-        data = pd.pivot_table(plot_df, values="Amount", index=[col1],
-            columns=["Date"])
+        data = pd.pivot_table(plot_df, values="Order ID", index=[col1],
+            columns=["Start Date"], aggfunc=len, fill_value=0)
     return data
 
-def make_chart(data,list, type,drugs):
+def make_chart(data, list, type, drugs):
     data = data.reset_index()
-    if type != "Antibiotic":
-        plot_df = data[data[["Antibiotic", type]].apply(tuple,1).isin(list)]
+    if type != "Order Name":
+        plot_df = data[data[["Order Name", type]].apply(tuple,1).isin(list)]
         chart = (
             alt.Chart(plot_df)
             .mark_line(opacity=0.4)
             .encode(
-                x="Date:T",
-                y=alt.Y("Amount:Q",stack=None, aggregate="sum"),
+                x="Start Date:T",
+                y=alt.Y("Order:Q",stack=None, aggregate="sum"),
                 color=type+":N"
             )
             .interactive()
         )
         return chart
     else:
-        plot_df = data[data["Antibiotic"].isin(drugs)]
+        plot_df = data[data["Order Name"].isin(drugs)]
         chart = (
             alt.Chart(plot_df)
             .mark_line(opacity=0.4)
             .encode(
-                x="Date:T",
-                y=alt.Y("Amount:Q",stack=None),
+                x="Start Date:T",
+                y=alt.Y("Order:Q",stack=None),
                 color=type+":N"
             )
             .interactive()
